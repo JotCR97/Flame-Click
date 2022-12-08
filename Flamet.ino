bc0fe8360ed536ec9409ae30d712745b196d6dc7
JotCR97 <110689057+JotCR97@users.noreply.github.com>
2022-12-08 17:44:52 -0600

    Add files via upload

---
+++ Flamet.ino
@@ -0,0 +1,86 @@
+#define flame_TEST
+#ifdef flame_TEST
+#include <ArduinoUnit.h>
+#endif
+//Permitir el uso de printf() con Serial para iniciar sesión.
+#include "log.h"
+//Libreria MIKROE Flame Click
+#include "flame.h"
+
+    //VARIABLES
+static flame_t flame;
+static log_t logger;
+
+static uint8_t flame_state = 0;
+static uint8_t flame_state_old = 1;
+
+    //FUNCIONES DE LA APLICACIÓN
+void setup() { 
+    Serial.begin(9600);
+    pinMode(PIN_LED, OUTPUT);
+}
+   {
+    log_cfg_t log_cfg;
+    flame_cfg_t cfg;
+    
+    LOG_MAP_USB_UART( log_cfg );
+    log_init( &logger, &log_cfg );
+    log_info(&logger, "---- Inicialización de la aplicación ----");
+
+    //  Haga clic en inicialización.
+    flame_cfg_setup( &cfg );
+    FLAME_MAP_MIKROBUS( cfg, MIKROBUS_1 );
+    flame_init( &flame, &cfg );
+}
+
+
+    //  Implementación de tareas.
+void application_task ( void )
+{
+    //  Estado del sensor
+    
+    flame_state = flame_check_status ( &flame );
+
+    //  Si el estado devuelve 1, el sensor detecto fuego 
+    if ( ( flame_state == 1 ) && ( flame_state_old == 0) )
+    {
+        log_printf( &logger, "  ~  C   ~ \r\n " );
+       
+        flame_state_old = 1;
+    }
+    
+    //  Si el estado devuelve 0, no se esta detectando fuego
+    if ( ( flame_state == 0 ) && ( flame_state_old == 1 ) )
+    {
+        log_printf( &logger, "   SIN LLAMA  \r\n " );
+        flame_state_old = 0;
+    }
+}
+
+
+
+void loop() {
+    application_task(); 
+    delay(1);  // Retraso entre lecturas para la estabilidad
+
+#ifndef UNIT_TEST {
+  if ( ( flame_state == 1 ) && ( flame_state_old == 0) )
+  {
+    toggleLed();
+    digitalWrite(PIN_LED, ledStatus);
+  }
+#else
+    Test::run();
+#endif
+}
+ 
+ 
+/*****************************************
+ * Función: toggleLed
+ * Descripción: Esta función cambia el estado del led (ledStatus),
+ * es decir si es LOW lo cambia a HIGH y viceversa.
+ *****************************************/
+void toggleLed(void)
+{
+  ledStatus =  !ledStatus;
+}
